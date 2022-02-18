a02cb84298b18e66b79d04174f8d3fed2541b216
vidinski <pvidinski@gmail.com>
2022-02-17 22:33:48 -0600

    Created applied force object

--- Dynamics/ForcesTypes.py
+++ Dynamics/ForcesTypes.py
@@ -7,5 +7,15 @@ class GravityForce():
             self.force_direction = force_direction
 
 
-                           
+class AppliedForce(): 
+        def __init__(self, index = 0, 
+                           time = 0.0):
+            self.t = time 
+            self.position_body = np.matrix([[self.t],
+                                            [self.t],
+                                            [0.0]])
+            self.dir_body = np.matrix([[0.0], [0.0],[-1.0*self.t]])
+            self.force_mag = self.t
+            self.torque_body = np.matmul(skewsym(self.position_body),force_mag*dir_body)
+                                      
 
