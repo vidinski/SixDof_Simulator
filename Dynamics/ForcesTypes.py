5f7b89b131f55dbe6b58b5f76eb86de1a1fe12f8
vidinski <pvidinski@gmail.com>
2022-05-21 23:28:48 -0500

    Adding spring force class

--- Dynamics/ForcesTypes.py
+++ Dynamics/ForcesTypes.py
@@ -21,8 +21,23 @@ class AppliedForce():
         def Update(self, new_time, A):
             self.t = new_time
             self.position_body = np.matrix([[0.1*self.t], [0.1*self.t],[0.1]])
-            self.force_mag = 10.0*self.t
+            self.force_mag = -10.0*self.t
             self.force_body = self.force_mag*self.dir_body
             self.torque_body = np.matmul(kine.skewsym(self.position_body),self.force_mag*self.dir_body)
             self.force_global = np.matmul(A, self.force_body)
             self.position_global = np.matmul(A,self.position_body)
+
+class SpringForce(): 
+        def __init__(self, index = 0):
+            self.loc_body = np.matrix([[0.0], [0.0],[0.0]])
+	    self.loc_ground = np.matrix([[0.0], [0.0],[0.0]])
+            self.force_body =  np.matrix([[0.0], [0.0],[0.0]])
+            self.torque_body =  np.matrix([[0.0], [0.0],[0.0]])
+            self.force_global = np.matrix([[0.0], [0.0],[0.0]])
+                                      
+        def Update(self, loc_ground, loc_body, A):
+            self.force_body =  np.matrix([[0.0], [0.0],[0.0]])
+            self.torque_body =  np.matrix([[0.0], [0.0],[0.0]])
+            self.force_global = np.matmul(A, self.force_body)
+	    
+
