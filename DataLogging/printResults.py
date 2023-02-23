efcd206d6061b78eaf6d9d47d428127518f6176c
vidinski <pvidinski@gmail.com>
2023-02-22 22:02:30 -0700

    deleting comments

--- DataLogging/printResults.py
+++ DataLogging/printResults.py
@@ -2,10 +2,6 @@ from Solver import solver
 import numpy as np
 
 def writeToFile(data):
-
-    # solver.textFile.write('test')
-    # solver.textFile.write('\n')
-
     for i in range (0,np.size(data)): 
         solver.textFile.write(str(data[0,i]))
         solver.textFile.write('\t')
